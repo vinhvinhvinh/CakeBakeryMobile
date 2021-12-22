0e14161672bbd0e53e4f31caead6cd34aab095f0
B.Tuyen <79151413+Tuyen1612@users.noreply.github.com>
2021-12-23 01:41:29 +0700

    add ProductDetail Screen

--- flutter_application_cake_bakery/lib/main.dart
+++ flutter_application_cake_bakery/lib/main.dart
@@ -6,6 +6,7 @@ import 'package:flutter_application_cake_bakery/screens/account/account_creen.da
 import 'package:flutter_application_cake_bakery/screens/account/profile/my_profile_screen.dart';
 import 'package:flutter_application_cake_bakery/screens/login/login_screen.dart';
 import 'package:flutter_application_cake_bakery/screens/main/main_screen.dart';
+import 'package:flutter_application_cake_bakery/screens/product_detail/product_detail_screen.dart';
 import 'screens/account/setting_and_security/password_change/password_change_screen.dart';
 import 'screens/account/setting_and_security/setting_security_screen.dart';
 import 'screens/changepassword/change_pasword_screen.dart';
@@ -34,7 +35,7 @@ class CakeBakery extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       routes: {
-        '/': (context) => const FavoriteScreen(),
+        '/': (context) => const ProductDetail(),
         '/main_screen': (context) => MainScreen(),
         '/signup': (context) => const SignUpScreen(),
         '/forget_password': (context) => const ForgetPassword(),
