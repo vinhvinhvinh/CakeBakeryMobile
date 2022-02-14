73d4e46d18c54dc35d44e6983c25aba26ed388a8
B.Tuyen <79151413+Tuyen1612@users.noreply.github.com>
2022-02-15 00:32:00 +0700

    load product by type

--- flutter_application_cake_bakery/lib/screens/home/provider/product_provider.dart
+++ flutter_application_cake_bakery/lib/screens/home/provider/product_provider.dart
@@ -4,12 +4,8 @@ import 'package:flutter_application_cake_bakery/services/product_service.dart';
 
 //San pham provider
 class ProductProvider extends ChangeNotifier {
-<<<<<<< HEAD
-  
-=======
   List<Product> bestSellingproducts = [];
   List<Product> newProducts = [];
->>>>>>> 4f807f0c6124834e4c0a4eae1e61b39d608ff9a5
   List<Product> products = [];
   List<Product> topProducts = [];
   List<Product> byProductTypeId = [];
